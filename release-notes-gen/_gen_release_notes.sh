668f94c2b6e0336c8987d9464e663ac612da0c09
ghjang <jmxexpert@hotmail.com>
2024-01-16 20:02:50 +0900

    줄분리 문자 추가, 개별 릴리즈 노트 저장용 폴더가 없을 경우 폴더를 생성하게 함.

--- release-notes-gen/_gen_release_notes.sh
+++ release-notes-gen/_gen_release_notes.sh
@@ -11,34 +11,43 @@ for VAR in $REQUIRED_VARS; do
     exit 1
   fi
 done
+echo "OK"
 
-printf "\n==== Set Environment Variables for Release Notes File ===="
+printf "\n==== Set Environment Variables for Release Notes File ====\n"
 if [ -z "$RELEASE_NOTES_DIR" ]; then
   RELEASE_NOTES_DIR="_release-notes"
 fi
+
+# Create the directory if it does not exist
+if [ ! -d "$RELEASE_NOTES_DIR" ]; then
+  mkdir -p $RELEASE_NOTES_DIR
+fi
+
 if [ -z "$COMBINED_RELEASE_NOTES_FILE" ]; then
   COMBINED_RELEASE_NOTES_FILE="RELEASES_NOTES.md"
 fi
+
 if [ -z "$TARGET_BRANCH" ]; then
   TARGET_BRANCH="main"
 fi
-echo "RELEASE_NOTES_DIR: $RELEASE_NOTES_DIR"
-echo "COMBINED_RELEASE_NOTES_FILE: $COMBINED_RELEASE_NOTES_FILE"
-echo "TARGET_BRANCH: $TARGET_BRANCH"
 
 TMP_RELEASE_NOTE_FILE="${RELEASE_NOTES_DIR}/${TAG_VERSION}_TMP.md"
 RELEASE_NOTE_FILE="${RELEASE_NOTES_DIR}/${TAG_VERSION}.md"
+
+echo "RELEASE_NOTES_DIR: $RELEASE_NOTES_DIR"
+echo "COMBINED_RELEASE_NOTES_FILE: $COMBINED_RELEASE_NOTES_FILE"
+echo "TARGET_BRANCH: $TARGET_BRANCH"
 echo "TMP_RELEASE_NOTE_FILE: $TMP_RELEASE_NOTE_FILE"
 echo "RELEASE_NOTE_FILE: $RELEASE_NOTE_FILE"
 
-printf "\n==== Set up GitHub CLI Auth Token ===="
+printf "\n==== Set up GitHub CLI Auth Token ====\n"
 export GH_TOKEN=$GITHUB_TOKEN
 
-printf "\n==== Get milestone number ===="
+printf "\n==== Get milestone number ====\n"
 MILESTONE_NUMBER=$(gh api repos/$GITHUB_REPOSITORY/milestones --jq ".[] | select(.title==\"$TAG_VERSION\") | .number")
 export MILESTONE_NUMBER
 
-printf "\n==== Get closed issues for the milestone ===="
+printf "\n==== Get closed issues for the milestone ====\n"
 if [ -n "$MILESTONE_NUMBER" ]; then
   ISSUES=$(curl -H "Authorization: token $GITHUB_TOKEN" \
                 -H "Accept: application/vnd.github.v3+json" \
@@ -61,7 +70,7 @@ else
   echo "[]" > issues.json
 fi
 
-printf "\n==== Create individual release notes ===="
+printf "\n==== Create individual release notes ====\n"
 echo "## $TAG_VERSION $(date +%Y-%m-%d)" > $TMP_RELEASE_NOTE_FILE
 echo "" >> $TMP_RELEASE_NOTE_FILE
 if [ $(jq length issues.json) -eq 0 ]; then
@@ -70,11 +79,11 @@ else
   jq -r '.[] | "- Issue #\(.number): \(.title)"' issues.json >> $TMP_RELEASE_NOTE_FILE
 fi
 
-printf "\n==== Add safe directory and check if inside Git work tree ===="
+printf "\n==== Add safe directory and check if inside Git work tree ====\n"
 git config --global --add safe.directory $(pwd)
 git rev-parse --is-inside-work-tree
 
-printf "\n==== Commit and push release notes ===="
+printf "\n==== Commit and push release notes ====\n"
 echo "pwd: $(pwd)"
 ls -al
 
@@ -87,7 +96,7 @@ git add $RELEASE_NOTE_FILE
 if [ "$(git diff --name-only --cached)" ]; then git commit -m "Add release notes for version $TAG_VERSION"; fi
 git push origin $TARGET_BRANCH
 
-printf "\n==== Create combined release notes ===="
+printf "\n==== Create combined release notes ====\n"
 REPO_NAME=$(echo "$GITHUB_REPOSITORY" | cut -d'/' -f2)
 echo "# '${REPO_NAME}' Release Notes" > $COMBINED_RELEASE_NOTES_FILE
 echo "" >> $COMBINED_RELEASE_NOTES_FILE
